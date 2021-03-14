a4bfb693cb874a6dfdbfda148269405d4a21e75e
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2021-03-14 23:10:28 +0100

    Implements Material Components

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -15,7 +16,7 @@ import com.example.affirmations.model.Affirmation
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
-    ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -23,6 +24,7 @@ class ItemAdapter(
     // Each data item is just an affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -42,6 +44,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
